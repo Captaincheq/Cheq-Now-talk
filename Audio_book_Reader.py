4b02fcd8c1e1daf652372ea194db4aa87a8066bf
Alec Mabhiza Chirawu <alecmabhizachirawu@gmail.com>
2021-03-19 17:02:17 +0800

    u

--- Audio_book_Reader.py
+++ Audio_book_Reader.py
@@ -2,15 +2,16 @@ import pyttsx3
 import PyPDF2
 
 #a pdf reader in python
-book = open('The Journey My Life.pdf','rb')
+book = open('Thesis 1.pdf','rb')
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 print(pages)
 
 speaker = pyttsx3.init()
 
-for num in range(2, pages):
-    page = pdfReader.getPage(2)
+
+for num in range(9,pages):
+    page = pdfReader.getPage(num)
     text = page.extractText()
     speaker.say(text)
 
